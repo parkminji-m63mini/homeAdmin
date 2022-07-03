e74221e431767475b2c9084d7a897e492399f7f3
parkminji-m63mini <68366848+parkminji-m63mini@users.noreply.github.com>
2022-07-03 09:41:02 +0900

    메인 테스트

--- src/com/mj/homeAdmin/HomeController.java
+++ src/com/mj/homeAdmin/HomeController.java
@@ -5,6 +5,7 @@
 
 package com.mj.homeAdmin;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,21 +20,107 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.mj.homeAdmin.checkList.model.service.checkListService;
+import com.mj.homeAdmin.checkList.vo.checkList;
+import com.mj.homeAdmin.comm.JavaUtil;
 import com.mj.homeAdmin.commn.service.CmmnServiceImpl;
+import com.mj.homeAdmin.manageM.model.service.ManegeMService;
+import com.mj.homeAdmin.manageM.vo.ManageM;
 import com.mj.homeAdmin.myinfo.model.service.MyinfoService;
 import com.mj.homeAdmin.myinfo.vo.MyinfoVo;
+import com.mj.homeAdmin.visit.model.service.VisitService;
+import com.mj.homeAdmin.visit.vo.Visit;
 
 @Controller
 public class HomeController
 {
 	@Autowired
 	private MyinfoService ms;
+	
+	@Autowired
+	private checkListService checkListService;
+
+	@Autowired
+	private ManegeMService manageService;
+	
+	@Autowired
+	private VisitService visitService;
+	
 	@Autowired
 	private CmmnServiceImpl cm;
 
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(MyinfoVo vo, Model model, HttpSession ss, HttpServletRequest req) throws Exception{
+		
+		//세션으로 가져오기
+		vo.setId((String)ss.getAttribute("ssID"));
+		
+		//세션에 필요한 정보가져오기
+				// 여기부터
+				 List<MyinfoVo> user = ms.selectUser(vo);
+				 
+				 
+				 
+				
+				 // 메인에 나타날 것들
+				 // 집 이미지
+				 String homeImg = ms.getHomeImg(user.get(0).getId());
+				 // 체크리스트
+				 System.out.println("확인");
+				 List<checkList> checkList = checkListService.selectCheckList(user.get(0).getId());
+				 List<checkList> checkListDetail = checkListService.selectCheckListDetail(user.get(0).getId());
+			
+				 // 공과금 전월vs당월
+				 
+				 String flag = "false";
+				 ManageM manageVo = new ManageM();
+				 manageVo.setuId(user.get(0).getId());
+				 String	yyyy= "" + LocalDate.now().getYear();
+		         String mm= JavaUtil.checkMM(""+LocalDate.now().getMonthValue(), "0");
+		        
+		        // 체크하는 용도로 사용함
+		        manageVo.setgChk(flag);
+		        
+		        
+		        manageVo.setYyyy(yyyy);
+		        manageVo.setMm(mm);
+		         System.out.println("manageVo.getuId()  " + manageVo.getuId());
+		         System.out.println("manageVo.getuId()  " + manageVo.getYyyy());
+		         System.out.println("manageVo.getuId()  " + manageVo.getMm());
+		        // 이번달
+		        List<ManageM> arrViewNow = manageService.manageIndex(manageVo);
+		        
+		        //-------------------------------//
+		        // 이번달, 이전달 비교
+		 
+		        manageVo.setMm2(JavaUtil.checkMM(mm, "1"));
+		        manageVo.setYyyy2(JavaUtil.checkYYYY(yyyy, mm, "1"));
+		        
+		        List<ManageM> arrViewPast = manageService.manageNP(manageVo);
+		        
+		        // 방명록
+				 List<Visit> visitList = visitService.mainVistList(user.get(0).getId());
+				
+				 vo.setId(user.get(0).getId());
+				 vo.setNm(user.get(0).getNm());
+				 vo.setnNm(user.get(0).getnNm());
+				 vo.setGb(user.get(0).getGb());
+			//	 System.out.println(user.get(0).getNm() + " 리스트 값");
+			//	 System.out.println(vo.getNm() + " vo값");
+				 
+				// 세션에 세팅
+				cm.goSessionChk(vo, ss, req);
+				cm.getSessionChk(ss);
+				model.addAttribute("user" ,vo);
+				model.addAttribute("homeImg", homeImg);
+				model.addAttribute("checkList", checkList);
+				model.addAttribute("checkListDetail", checkListDetail);
+				model.addAttribute("arrViewNow", arrViewNow);
+		        model.addAttribute("arrViewPast", arrViewPast);
+		        model.addAttribute("visitList", visitList);
+		
+		
 		return "../../main";
 	}
 	
